35a3e0bdc153c45c55c5e50982adbf0576807656
azimjonshodiyorov <azimjonshodiyorov@gmail.com>
2024-09-08 23:01:42 +0500

    add migrations

--- src/AnimalServices/Program.cs
+++ src/AnimalServices/Program.cs
@@ -1,15 +1,59 @@
 using AnimalServices.DataContext;
+using AnimalServices.Services;
+using AnimalServices.Services.Interfaces;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+ 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<RabbitDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+    );
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddScoped<IAnimalService, AnimalService>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+//config rabbitMq
+builder.Services.AddMassTransit(x =>
+    {
+        //outbox pattern
+        x.AddEntityFrameworkOutbox<RabbitDbContext>(o =>
+        {
+            //10 sonya kutish habar yuborishdan oldin
+            o.QueryDelay = TimeSpan.FromSeconds(10);
+            o.UsePostgres();
+            o.UseBusOutbox();
+        });
+        x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("animal", false));
+        
+        x.UsingRabbitMq((context, cfg) =>
+        {
+            cfg.Host(builder.Configuration["RabbitMq:Host"],"/", host =>
+            {
+                host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+                host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+            });
+            cfg.ConfigureEndpoints(context);
+        });
+    }
+);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("customPolicy", x =>
+    {
+        x.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials()
+            .WithOrigins("http://localhost:8081");
+    });
+});
+
 var app = builder.Build();
 
+app.UseCors("customPolicy");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -18,7 +62,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+//app.UseAuthorization();
+app.MapControllers();
 try
 {
     DbInitializer.InitDb(app);
