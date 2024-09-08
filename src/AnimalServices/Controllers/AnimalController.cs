f883a2687e02fe1c80913cfbaf1e6b8e5a58bc63
azimjonshodiyorov <azimjonshodiyorov@gmail.com>
2024-09-08 09:59:46 +0500

    finish controller

--- src/AnimalServices/Controllers/AnimalController.cs
+++ src/AnimalServices/Controllers/AnimalController.cs
@@ -1,3 +1,4 @@
+using AnimalServices.Dtos;
 using AnimalServices.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +14,30 @@ public class AnimalController : ControllerBase
     {
         _animalService = animalService;
     }
-    
-    
+
+    [HttpGet]
+    [Route("getAll")]
+    public async Task<List<AnimalDto>> GetAll() =>
+        await this._animalService.GetAllAnimals();
+
+    [HttpGet]
+    [Route("getById")]
+    public async Task<AnimalDto> GetById(Guid id) =>
+        await this._animalService.GetAnimalById(id);
+
+
+    [HttpPost]
+    [Route("add")]
+    public async Task<AnimalDto> AddAnimal(CreateAnimalDto animalDto) =>
+        await this._animalService.CreateAnimal(animalDto);
+
+    [HttpPut]
+    [Route("update")]
+    public async Task UpdateAnimal(Guid id, UpdateAnimalDto animalDto) =>
+        await this._animalService.UpdateAnimal(id,animalDto);
+
+    [HttpDelete]
+    [Route("deleteById")]
+    public async Task DeleteAnimal(Guid id) =>
+        await this._animalService.DeleteAnimal(id);
 }
